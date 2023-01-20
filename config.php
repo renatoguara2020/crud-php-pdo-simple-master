44eb292723b2c108935b7b51f231d2ada43c7e1b
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-20 19:59:06 -0300

    ok

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 
-$databaseHost = 'localhost';
+$host = 'localhost';
 $databaseName = 'test1';
-$databaseUsername = 'root';
-$databasePassword = '';
+$username = 'root';
+$password = '';
 
 try {
 	// http://php.net/manual/en/pdo.connections.php
-	$conn = new PDO("mysql:host={$databaseHost};dbname={$databaseName}", $databaseUsername, $databasePassword);
+	$conn = new PDO("mysql:host={$host};dbname={$databaseName}", $username, $password);
 	
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Setting Error Mode as Exception
 	// More on setAttribute: http://php.net/manual/en/pdo.setattribute.php
