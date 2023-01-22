54491445a327990a74de6d48669e6ef606819781
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-22 09:20:37 -0300

    ok

--- link.php
+++ link.php
@@ -1,3 +1,4 @@
 <?php
 
-echo "<td><a href=\"edit.php?id=$row[id]\">Update</a> | <a href=\"delete.php?id=$row[id]\"></a></td>";		
+echo "<td><a href=\"edit.php?id=$row[id]\">Update</a> | <a href=\"delete.php?id=$row[id]\"></a></td>";
+echo "<a href=\"edit.php?id=$row[id]\">Update</a> | <a href=\"delete.php?id=$row[id]\"></a>";
