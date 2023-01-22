6cc5f7fb5863932dc918c5647c97ebf3ece9a2e2
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-22 12:16:48 -0300

    ok

--- ExampleEdit.php
+++ ExampleEdit.php
@@ -111,5 +111,6 @@ while($row = $query->fetch(PDO::FETCH_ASSOC)){
 </html>
 
 
-<a href=\"edit.php?id=$row[id]\">Update</a> | <a href=\"delete.php?id=$row[id]\">Delete</a>
+echo '<a href=\"edit.php?id=$row[id]\">Update</a> | <a href=\"delete.php?id=$row[id]\">Delete</a>';
 <a href="delete.php?id=$row['id']"></a>
+<a href="edit.php?id=$row['id']"></a>
